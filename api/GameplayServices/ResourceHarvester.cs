f273ec02d25c50b6fbc2e4c64fef1ff2082bba1e
Pybounce <pybounce@protonmail.com>
2024-11-07 17:37:01 +0000

    cleanup

--- api/GameplayServices/ResourceHarvester.cs
+++ api/GameplayServices/ResourceHarvester.cs
@@ -7,22 +7,16 @@ public interface IResourceHarvester {
 
 public class ResourceHarvester: IResourceHarvester, IDisposable {
     private int? _resourceId { get; set; }
-    private readonly IMessageWriter _messageWriter;
     private readonly IEventHub _eventHub;
     private readonly IScopedTickSystem _scopedTickSystem;
-    
+
     public ResourceHarvester(
         IScopedTickSystem scopedTickSystem, 
-        IMessageWriter messageWriter, 
         IEventHub eventHub) 
         {
-        _messageWriter = messageWriter;
         _eventHub = eventHub;
         _scopedTickSystem = scopedTickSystem;
         _scopedTickSystem.OnTick += OnTick;
-
-        _eventHub.Subscribe<ItemCollectedEvent>(Subbed);
-        _eventHub.Subscribe<ItemCollectedEvent>(Subbed2);
     }
 
     public bool TryStartResourceHarvest(int resourceId) {
@@ -37,26 +31,13 @@ public class ResourceHarvester: IResourceHarvester, IDisposable {
     }
 
     private void OnTick() {
-        //Console.WriteLine("harvester ontick");
         if (_resourceId != null) {
             _eventHub.Publish(new ItemCollectedEvent(_resourceId.Value, 1));
-
-            //var message = new ItemCollected(_resourceId.Value, 1);
-            //_messageWriter.AddMessage(message);
         }
     }
 
     public void Dispose() {
         _scopedTickSystem.OnTick -= OnTick;
-        _eventHub.Unsubscribe<ItemCollectedEvent>(Subbed);
-        _eventHub.Unsubscribe<ItemCollectedEvent>(Subbed2);
-    }
-
-    private void Subbed(ItemCollectedEvent e) {
-        Console.WriteLine("item collected event sub");
-    }
-    private void Subbed2(ItemCollectedEvent e) {
-        Console.WriteLine("item collected event sub2");
     }
 
 }
