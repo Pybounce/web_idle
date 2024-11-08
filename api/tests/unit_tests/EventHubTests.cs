445a5e7f609d8dd722d1635b080a447bd36876e0
Pybounce <pybounce@protonmail.com>
2024-11-08 14:19:45 +0000

    added test project with eventhub tests

---
+++ api/tests/unit_tests/EventHubTests.cs
@@ -0,0 +1,79 @@
+namespace unit_tests;
+
+using Moq;
+
+
+
+[TestFixture]
+public class EventHubTests {
+
+
+    public class SomeType {
+        public int SomeInt { get; set; }
+    }
+    public class SomeOtherType {
+        public int SomeOtherInt { get; set; }
+    }
+
+    private EventHub _eventHub;
+    [SetUp]
+    public void SetUp() {
+        _eventHub = new EventHub();
+    }
+
+    [TearDown]
+    public void TearDown() {
+
+    } 
+    
+    [Test]
+    public void Publishing_To_One_Subscriber___One_Subscriber_Invoked() {
+
+        var mockHandler = new Mock<Action<SomeType>>();
+        var someInt = 1;
+        _eventHub.Subscribe<SomeType>(mockHandler.Object);
+
+        _eventHub.Publish<SomeType>(new SomeType { SomeInt = someInt });
+
+        mockHandler.Verify(h => h(It.Is<SomeType>(st => st.SomeInt == someInt)), Times.Once);
+    }
+
+    [Test]
+    public void Publishing_To_Many_Subscribers_Same_Type___Many_Subscribers_Invoked() {
+                
+        var mockHandler = new Mock<Action<SomeType>>();
+        var someInt = 1;
+        _eventHub.Subscribe<SomeType>(mockHandler.Object);
+        _eventHub.Subscribe<SomeType>(mockHandler.Object);
+
+        _eventHub.Publish<SomeType>(new SomeType { SomeInt = someInt });
+
+        mockHandler.Verify(h => h(It.Is<SomeType>(st => st.SomeInt == someInt)), Times.Exactly(2));
+    }
+
+    [Test]
+    public void Publishing_To_Many_Subscribers_Different_Type___Only_Same_Type_Subscribers_Invoked() {
+        
+        var someMockHandler = new Mock<Action<SomeType>>();
+        var someOtherMockHandler = new Mock<Action<SomeOtherType>>();
+        var someInt = 1;
+        _eventHub.Subscribe<SomeType>(someMockHandler.Object);
+        _eventHub.Subscribe<SomeOtherType>(someOtherMockHandler.Object);
+
+        _eventHub.Publish<SomeType>(new SomeType { SomeInt = someInt });
+
+        someMockHandler.Verify(h => h(It.Is<SomeType>(st => st.SomeInt == someInt)), Times.Once);
+        someOtherMockHandler.Verify(h => h(It.Is<SomeOtherType>(st => st.SomeOtherInt == someInt)), Times.Never);
+    }
+
+    [Test]
+    public void Publishing_To_Zero_Subscribers___No_Exceptions_Thrown() {
+        
+        var someInt = 1;
+
+        _eventHub.Publish<SomeType>(new SomeType { SomeInt = someInt });
+
+        Assert.Pass("No exceptions were thrown");
+    }
+
+}
