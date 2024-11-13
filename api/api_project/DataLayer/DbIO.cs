1945f949e34fc42023a645a4d8283afd0a103cdd
Pybounce <pybounce@protonmail.com>
2024-11-13 23:33:25 +0000

    cosmosdb client with removed appsettings

--- api/api_project/DataLayer/DbIO.cs
+++ api/api_project/DataLayer/DbIO.cs
@@ -1,4 +1,7 @@
 
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
 
 public interface IDbIO {
 
@@ -6,4 +9,15 @@ public interface IDbIO {
 
 public class DbIO: IDbIO {
     
+    private readonly CosmosClient _dbClient;
+
+    public DbIO() {
+        _dbClient = CreateDbClient();
+    }
+
+    private CosmosClient CreateDbClient() {
+        TokenCredential credential = new DefaultAzureCredential();
+        return new CosmosClient("AccountEndpoint=https://skybounce-cosmosdb-web-idle.documents.azure.com:443/;AccountKey=E3HOo5nH8lHHXkK7h0icLPmJkSa5bJUZE9Nwuhz2KuYuidpIY8DA0xQpZZ29Mv7THlDKdhawJp0TACDbmo0fTA==;", credential);
+    }
+
 }
